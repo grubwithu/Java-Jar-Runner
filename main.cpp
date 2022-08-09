de70146f4184fe2ff0b0abadb7dd8512598970e4
grubwithu <56990954+grubwithu@users.noreply.github.com>
2022-08-09 20:30:11 +0800

    修复了文件名中含空格的情况

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     if(argc != 2)return 0;
-    char front[100] = "start /B javaw -jar ";
+    char front[1024] = "start /B javaw -jar \"";
     char *str = strcat(front,argv[1]);
+    int length = static_cast<int>(strlen(str));
+    str[length] = '\"';
+    str[length+1] = '\0';
     system(str);
     return 0;
 }
